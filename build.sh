4ceb3218829382940786d6ba27e0d3b841892eca
Michael Ferguson <mfergs7@gmail.com>
2023-11-17 17:49:25 -0500

    fix build

--- build.sh
+++ build.sh
@@ -25,16 +25,6 @@ else
 fi
 echo "ROS version is: "$ROS_VERSION
 
-# clear `build/` folder.
-# TODO: Do not clear these folders, if the last build is based on the same ROS version.
-rm -rf ../../build/
-rm -rf ../../devel/
-rm -rf ../../install/
-# clear src/CMakeLists.txt if it exists.
-if [ -f ../CMakeLists.txt ]; then
-    rm -f ../CMakeLists.txt
-fi
-
 # exit
 
 # substitute the files/folders: CMakeList.txt, package.xml(s)
@@ -51,20 +41,3 @@ elif [ ${ROS_VERSION} = ${VERSION_ROS2} ]; then
     cp -rf launch_ROS2/ launch/
 fi
 
-# build
-pushd `pwd` > /dev/null
-if [ $ROS_VERSION = ${VERSION_ROS1} ]; then
-    cd ../../
-    catkin_make -DROS_EDITION=${VERSION_ROS1}
-elif [ $ROS_VERSION = ${VERSION_ROS2} ]; then
-    cd ../../
-    colcon build --cmake-args -DROS_EDITION=${VERSION_ROS2} -DHUMBLE_ROS=${ROS_HUMBLE}
-fi
-popd > /dev/null
-
-# remove the substituted folders/files
-if [ $ROS_VERSION = ${VERSION_ROS2} ]; then
-    rm -rf launch/
-fi
-
-popd > /dev/null
